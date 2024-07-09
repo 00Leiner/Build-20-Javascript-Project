9c94406ebf059a2c8c1dc63d6f74303601a8f667
Leiner <leinersacdalan4@gmail.com>
2024-07-09 14:31:35 +0800

    Bell Notification

--- Jumping-Letter-Html-Project/app.js
+++ Jumping-Letter-Html-Project/app.js
@@ -3,5 +3,8 @@ const letters = document.querySelectorAll(".letters span");
 letters.forEach((letter) => {
     letter.addEventListener("click",(e) => {
         e.target.classList.add("active");
+        setTimeout(() => {
+            e.target.classList.remove("active");
+        }, 2000); // Duration matches the animation duration
     });
 });
